216a88c620cee0a937478a515f6a5114484fb634
韩陵山人 <q2719833@126.com>
2016-08-05 13:37:10 +0800

    “看毛片”算法

---
+++ kmp.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+void getNext(const char *p, int *next)
+{
+	int i, k;  //i为下标,k为next[i]的值. i在递增,k回溯
+	int slen = strlen(p); //要查找的串的长度.
+	next[0] = 0; //p[0]的next值为0;
+
+	for (i = 1, k = 0; i < slen; ++i) {
+		while (k != 0 && p[i] != p[k])
+			k = next[k - 1];
+
+		if (p[i] == p[k])
+			k++;
+
+		next[i] = k;
+	}
+}
+
+int kmp(const char *t, const char *p, int *next)
+{
+	int i, q;
+	int tlen = strlen(t);
+	int plen = strlen(p);
+
+	getNext(p, next);
+
+	for (i = 0, q = 0; i < tlen; i++) {
+		while (q > 0 && p[q] != t[i])
+			q = next[q - 1];
+
+		if (t[i] == p[q])
+			q++;
+
+		if (q == plen)
+			return (i - q + 1);
+	}
+	return 0;
+}
+
+int main(void)
+{
+	int i, len;
+	int next[20]={0};
+	char T[] = "abcdabcbabcdabdsss";
+	char P[] = "abcdabd";
+	printf("%s\n",T);
+	printf("%s\n",P );
+	int res = kmp(T, P, next);
+	len = strlen(P);
+	printf("index:%d\n", res);
+	for (i = 0; i < len; ++i)
+	{
+		printf("%d ",next[i]);
+	}
+	printf("\n");
+	return 0;
+}

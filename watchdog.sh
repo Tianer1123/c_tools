174052fe1df08a8dc1df821323020283f40280da
韩陵山人 <q2719833@126.com>
2016-08-22 15:21:34 +0800

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -5,7 +5,7 @@ PNum=2      #开启两个进程（父子进程）
 #grep -w 精确匹配, grep -v grep 过滤掉包含grep的行， wc -l显示文件的行数
 
 #根据进程名查找到所有的进程ID，并kill掉所有的进程。
-PID=`ps -ef | grep -w $PROC_NAME | grep -v "$0" | grep -v grep | awk '{print $2}'`
+PID=`ps -ef | grep -w $PROC_NAME | grep -v grep | awk '{print $2}'`
 echo $PID
 for id in $PID
 do

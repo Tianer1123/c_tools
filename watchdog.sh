efd9dbe15256265d74b804645be22600dcfae9b3
韩陵山人 <q2719833@126.com>
2016-08-22 15:00:57 +0800

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -3,7 +3,7 @@ PROC_NAME=/home/tianer/opt/test/a.out
 PNum=2      #开启两个进程（父子进程）
 
 #根据进程名查找到所有的进程ID，并kill掉所有的进程。
-PID=`ps -ef | grep $PROC_NAME| grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+PID=`ps -ef | grep $PROC_NAME | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
 echo $PID
 for id in $PID
 do
@@ -13,7 +13,7 @@ done
 #查找该进程的个数，如果与预设的不一致，killall掉所有进程并重启。
 while :
 do
-	ProcNumber=`ps -ef |grep -w $PROC_NAME|grep -v grep|wc -l`
+	ProcNumber=`ps -ef | grep -w $PROC_NAME | grep -v grep | wc -l`
 	if [ $ProcNumber -lt $PNum ];then
 		killall -9 $PROC_NAME
 		$PROC_NAME &

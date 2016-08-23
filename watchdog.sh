6d28f4937f3b542be8ccd78f29f7cdfdbeb2d024
韩陵山人 <q2719833@126.com>
2016-08-23 17:47:39 +0800

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -2,6 +2,24 @@
 PROC_NAME=/home/tianer/opt/test/a.out
 PNum=2      #开启两个进程（父子进程）
 
+CONF_FILE=/root/test.conf
+#读取配置中的PNum
+#去掉空行和#开头的注释
+egrep -v "(^$)|(^#)" $CONF_FILE | while read line; do
+
+name=`echo $line|awk -F '=' '{print $1}'`
+value=`echo $line|awk -F '=' '{print $2}'`
+
+if [ $name = "process_num" ];then
+        #$((0x01)) 16进制转10进制,$[$a + $b]计算两个数字的和
+        PNum=$[$(($value))+1]
+        echo "$PNum"
+        break
+else
+        echo "$PNum"
+fi
+done
+
 #grep -w 精确匹配, grep -v grep 过滤掉包含grep的行， wc -l显示文件的行数
 
 #根据进程名查找到所有的进程ID，并kill掉所有的进程。

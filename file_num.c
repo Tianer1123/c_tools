c8fd849cd507f965765c246c7fb0b8376aa9cb25
Tianer1123 <q2719833@126.com>
2016-10-25 14:05:41 +0800

    代码量统计

--- file_num.c
+++ file_num.c
@@ -5,48 +5,67 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 #define PATH_SIZE 512
 #define FILE_PATH_SIZE 1024
+#define MAX_LINE_SIZE FILE_PATH_SIZE
 
-int get_line_num(char *tmp_path)
+int get_line_num(char *path)
 {
-	return 0;
+	FILE *fp = NULL;
+	int num = 0;
+	char line[MAX_LINE_SIZE] = {'\0'};
+
+	fp = fopen(path, "r");
+	if (!fp) {
+		printf("error: open file %s failed!\n", path);
+		return 0;
+	}
+
+	while (fgets(line, MAX_LINE_SIZE, fp)) {
+		num++;
+	}
+
+	fclose(fp);
+
+	return num;
 }
 
-void get_num(char *path, int *line_number)
+void get_num(char *path, int *line_num)
 {
 	char tmp_path[FILE_PATH_SIZE] = {'\0'};
-	int num = 0;
 	DIR *dir = NULL;
 	struct dirent *entry = NULL;
 	struct stat st;
 
 	if ((dir = opendir(path)) == NULL) {
-		printf("open dir failed!\n");
+		perror("opendir");
 		return;
 	}
 
 	while((entry = readdir(dir))) {
-		if (lstat(entry->d_name, &st) == -1) {
-			printf("can't get the stat of file %s.\n", entry->d_name);
+
+		snprintf(tmp_path, FILE_PATH_SIZE, "%s/%s", path, entry->d_name);
+
+		if (lstat(tmp_path, &st) == -1) {
+			perror("lstat");
 			continue;
 		}
 
 		if ((st.st_mode & S_IFMT) == S_IFDIR) {
-			/* . ..目录过滤掉 */
+			/* 过滤掉. ..目录 */
 			if (!(memcmp(entry->d_name, ".", 1)) || !(memcmp(entry->d_name, "..", 2))) {
 				continue;
 			}
-			printf("----%s is a dir!\n", entry->d_name);
+			get_num(tmp_path, line_num);
 		} else {
 			/* 以.开头的隐藏文件不统计 */
 			if (!(memcmp(entry->d_name, ".", 1))) {
 				continue;
 			}
-			num = get_line_num(tmp_path);
+			*line_num += get_line_num(tmp_path);
 		}
-		printf("file name:%s\n", entry->d_name);
 	}
 
 	closedir(dir);
@@ -54,7 +73,7 @@ void get_num(char *path, int *line_number)
 
 int main(int argc, char **argv)
 {
-	int line_nums = 0;
+	int line_num = 0;
 	char path[PATH_SIZE] = {'\0'};
 	if (argc < 2) {
 		printf("USEAGE: ./a.out [path]!\n");
@@ -68,7 +87,9 @@ int main(int argc, char **argv)
 		exit(-1);
 	}
 
-	get_num(path, &line_nums);
+	get_num(path, &line_num);
+
+	printf("all line num is : %d\n", line_num);
 
 	return 0;
 }
